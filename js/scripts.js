2c35ce91756f616cd08a318c13816fc21bf71886
rajkumarst <39277662+rajkumarst@users.noreply.github.com>
2024-03-17 03:10:02 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector('#sideNav');
+  if (sideNav) {
+      new bootstrap.ScrollSpy(document.body, {
+          target: '#sideNav',
+          rootMargin: '0px 0px -40%',
+      });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+          if (window.getComputedStyle(navbarToggler).display !== 'none') {
+              navbarToggler.click();
+          }
+      });
+  });
+
+});
+
+
+
+// Get the modal
+var modal = document.getElementById("proj1Modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("proj1Card");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
